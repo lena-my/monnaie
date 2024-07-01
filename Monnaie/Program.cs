e0f99c2d0a330da34cb42251f3081728a05bef9b
Helena Miura <helenamiura@gmail.com>
2024-07-01 10:30:30 +0200

    added delegate method

---
+++ Monnaie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+    public delegate void ShowMoney(double somme);
+
+    public static void ShowDollar(double somme)
+    {
+        Console.WriteLine(somme + "$");
+    }
+
+    public static void ShowEuro(double somme)
+    {
+        Console.WriteLine(somme + "€");
+    }
+
+    public static void Main()
+    {
+        ShowMoney showMoney = ShowEuro;
+        showMoney(12);
+        showMoney = ShowDollar;
+        showMoney(12);
+
+
+    }
+}
